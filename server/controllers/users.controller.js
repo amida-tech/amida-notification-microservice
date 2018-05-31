da0d26d9f41ce321c874181e814d4a948c44834e
Elijah George <elijah@amida.com>
2018-05-31 12:26:59 -0400

    add user routes

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,30 @@
+import httpStatus from 'http-status';
+import Promise from 'bluebird';
+import db from '../../config/sequelize';
+import APIError from '../helpers/APIError';
+import Sequelize from 'sequelize';
+
+const User = db.User;
+const Op = Sequelize.Op;
+
+/**
+ * Start a new thread
+ * @property {string} req.body.username - Body of the message
+ * @returns {User}
+ */
+function create(req, res, next) {
+    const { username } = req.user;
+    User.findOrCreate({where: {username}})
+    .spread((user, created) => {
+      res.send({user})
+    });
+}
+
+function sendPushNotification(req, res, next) {
+    res.send(req.body);
+}
+
+export default {
+    create,
+    sendPushNotification
+};

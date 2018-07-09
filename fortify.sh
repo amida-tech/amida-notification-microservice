e4b43fe820de8eed1c91b8ffedd0355c901bb719
Amidabot <contact@amida.com>
2018-07-09 17:37:29 -0400

    fixed fortify script

--- fortify.sh
+++ fortify.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-PROJECT_NAME=sud-messaging-api
-PROJECT_VER=2.0
+PROJECT_NAME=SUD-notification-api
+PROJECT_VER=1.0
 
 RESULT_FILE=${PROJECT_NAME}-${PROJECT_VER}
 TEMP_FILE=${PROJECT_NAME}-${PROJECT_VER}-${BUILD_NUMBER}
+
+echo "You need yarn and fortify installed for this script to work."
+read -p "Continue? (Y/n): " continue
+if [[ "${continue:0:1}" == "n" || "${continue:0:1}" == "N" ]]; then
+	echo "Aborting..."
+	exit 1
+fi
+
+echo "=========================="
+echo "Installing dependencies"
+yarn 
+
+echo "=========================="
+echo "Copying default .env"
+cp .env.example .env
+
+echo "=========================="
+echo "Building..."
+yarn build
 
 echo "=========================="
 echo "Updating rulepacks"
@@ -16,18 +35,18 @@ sourceanalyzer -clean
 
 echo "=========================="
 echo "Scanning"
-sourceanalyzer -scan -f $TEMP_FILE.fpr -Xmx5529M -Xms400M -Xss24M ./app/*
+sourceanalyzer -scan -f $TEMP_FILE.fpr -Xmx8092M ./dist
 
 echo "=========================="
 echo "Merging"
 EXISTING_FPR_FILE=$(find ./fortify -name "*.fpr")
 if [ -z "${EXISTING_FPR_FILE}" ]; then
-    echo "No existing scan found. No merging performed."
-    mv $TEMP_FILE.fpr $RESULT_FILE.fpr
+	echo "No existing scan found. No merging performed."
+	mv $TEMP_FILE.fpr $RESULT_FILE.fpr
 else
-    echo "Merging existing fortify file $EXISTING_FPR_FILE into new scan."
-    FPRUtility -merge -project $EXISTING_FPR_FILE -source $TEMP_FILE.fpr -f $RESULT_FILE.fpr
-    rm $TEMP_FILE.fpr
+	echo "Merging existing fortify file $EXISTING_FPR_FILE into new scan."
+	FPRUtility -merge -project $EXISTING_FPR_FILE -source $TEMP_FILE.fpr -f $RESULT_FILE.fpr
+	rm $TEMP_FILE.fpr
 fi
 
 echo "=========================="

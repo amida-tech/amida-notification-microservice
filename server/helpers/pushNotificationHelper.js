ec527f55a5f25fffe550dea9aff2f4d12e8b44b8
Elijah George <elijah@amida.com>
2018-07-12 15:11:10 -0400

    modify payload to include all the data

--- server/helpers/pushNotificationHelper.js
+++ server/helpers/pushNotificationHelper.js
@@ -89,8 +89,8 @@ function sendPushNotification(receiver, data, req, res) {
         "show_in_foreground": true
     };
 
-    iosPushData = Object.assign(iosPushData, data);
-    androidPushData = Object.assign(androidPushData, data);
+    iosPushData = {...iosPushData, body: data};
+    androidPushData = {...androidPushData, body: data};
 
     receiver.Devices.forEach((device) => {
 

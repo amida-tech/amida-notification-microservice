6230e1dcf581f2ef71ffcab942711545c2d4e93d
Jonah Bregstone <jbregsto@oberlin.edu>
2018-10-09 16:41:20 -0400

    fixing reference path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import config from './config/config';
 import app from './config/express';
 /* eslint-disable no-unused-vars */
 import db from './config/sequelize';
-import logger from './winston';
+import logger from './config/winston';
 
 // make bluebird default Promise
 Promise = require('bluebird'); // eslint-disable-line no-global-assign

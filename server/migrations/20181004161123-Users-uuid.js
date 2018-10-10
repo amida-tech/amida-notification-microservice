46a649a9a1a986b0f8249e8e819c2fefc85bfb0f
Elijah George <elijah@amida.com>
2018-10-10 15:42:50 -0400

    do not migrate on startup

--- server/migrations/20181004161123-Users-uuid.js
+++ server/migrations/20181004161123-Users-uuid.js
@@ -6,18 +6,16 @@ module.exports = {
      Add altering commands here.
      Return a promise to correctly handle asynchronicity.
      */
-        return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;').then(() => queryInterface.addColumn('Users', 'uuid',
+        return queryInterface.addColumn('Users', 'uuid',
             {
                 type: Sequelize.UUID,
                 defaultValue: null,
                 allowNull: true,
                 unique: true,
                 after: 'id',
-               // primaryKey: true,
-            })
-         ).then(() => {
-             queryInterface.addIndex('Users', ['uuid']);
-         });
+            }).then(() => {
+                queryInterface.addIndex('Users', ['uuid']);
+            });
     },
     down(queryInterface) {
         return [

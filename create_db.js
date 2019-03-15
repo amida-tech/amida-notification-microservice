09c67fed120ec57c0ce9ecf8c87afb6c79e5b172
Nick McCally <nick@amida.com>
2019-03-15 12:02:58 -0400

    Update

--- create_db.js
+++ create_db.js
@@ -1,7 +1,7 @@
 import pg from 'pg';
 import config from './config/config';
 
-const conStringPri = `postgres://${config.postgres.user}:${config.postgres.password}@${config.postgres.host}:${config.postgres.port}/postgres`;
+const conStringPri = `postgres://{config.postgres.user}:{config.postgres.password}@{config.postgres.host}:{config.postgres.port}/postgres`;
 
 pg.connect(conStringPri, (err, client, done) => { // eslint-disable-line no-unused-vars
     if (err) {
@@ -9,7 +9,7 @@ pg.connect(conStringPri, (err, client, done) => { // eslint-disable-line no-unus
         process.exit(1);
     }
     // create the db and ignore any errors, for example if it already exists.
-    client.query(`CREATE DATABASE "${config.postgres.db}";`, (err1) => { // eslint-disable-line no-unused-vars
+    client.query(`CREATE DATABASE "{config.postgres.db}";`, (err1) => { // eslint-disable-line no-unused-vars
         // If in test mode and a database exists don't let it get created
         if (err1) {
             console.log(`Database Creation Failed. Please check to see if the database "${config.postgres.db}" already exists if so please delete it`); // eslint-disable-line no-console

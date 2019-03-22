ba8cb744c9d8719c04e7c39b1519ff7bcb5595e2
Grant Orndorff <grant@amida.com>
2019-03-22 17:05:44 -0400

    await/async-ifying and mongo-ifying create_missing_users.js

--- tools/createMissingUsers.js
+++ tools/createMissingUsers.js
@@ -1,62 +1,47 @@
-import db from '../sequelize';
-const User = db.User;
-import config from '../../config/config';
-import request from 'request';
-import uuidv4 from 'uuid/v4';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import fetch from 'node-fetch';
+import config from '../config/config';
+import { User } from '../server/models/user.model';
 
+(async () => {
+    const loginResponse = await fetch(`${config.authServiceAPI}/auth/login`, {
+        method: 'post',
+        body: JSON.stringify({
+            username: config.pushNotificationsServiceUserUsername,
+            password: config.pushNotificationsServiceUserPassword,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (loginResponse.status !== 200 && loginResponse.status !== 201) {
+        throw Error('Could Not Authenticate as Admin Push Notification Service User');
+    }
+    const adminToken = (await loginResponse.json()).token;
 
-const makeRequest = (data, callback) => {
-    request.post(data, callback);
-};
-const getRequest = (data, callback) => {
-    request.get(data, callback);
-};
-
-const createUsers = () => new Promise((resolve, reject) => {
-        makeRequest({
-            url: `${config.authServiceAPI}/auth/login`,
-            body: {
-                username: config.pushNotificationsServiceUserUsername,
-                password: config.pushNotificationsServiceUserPassword,
-            },
-            json: true,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }, (err, httpResponse) => {
-            if (err) {
-                throw err;
-            } else if (httpResponse.statusCode !== 200 && httpResponse.statusCode !== 201) {
-                throw Error('Could Not Authenticate as Admin');
-            }
-
-            const adminToken = httpResponse.body.token;
-          // get all users currently on authservice and save uuids for those
-          // on notification service
-            getRequest({
-                url: `${config.authServiceAPI}/user`,
-                headers: {
-                    Authorization: `Bearer ${adminToken}`,
-                    'Content-Type': 'application/json',
-                },
-                json: true,
-            }, (err, httpResponse) => {
-                if (err) {
-                    throw err;
-                } else if (httpResponse.statusCode !== 200 && httpResponse.statusCode !== 201) {
-                    throw Error(httpResponse.body.message);
-                }
-                const authUsers = httpResponse.body;
-
-
-                const createUserPromises = [];
-                authUsers.forEach((authUser) => {
-                    const { username, uuid } = authUser;
-                    createUserPromises.push(User.findOrCreate({ where: { username }, defaults: { uuid } }));
-                });
-                Promise.all(createUserPromises).then(() => resolve());
-            });
-        });
+    // get all users currently on authservice and save uuids for those
+    // on notification service
+    const usersResponse = await fetch(`${config.authServiceAPI}/user`, {
+        method: 'get',
+        headers: {
+            Authorization: `Bearer ${adminToken}`,
+            'Content-Type': 'application/json',
+        },
+    });
+    if (usersResponse.status !== 200 && usersResponse.status !== 201) {
+        throw Error((await usersResponse.json()).message);
+    }
+    const authUsers = await usersResponse.json();
+    const createUserPromises = authUsers.map(user => User.findOneAndUpdate(
+        { username: user.username },
+        { username: user.username, uuid: user.uuid },
+        { new: true, upsert: true }
+    ));
+    await Promise.all(createUserPromises);
+})()
+.then(process.exit)
+.catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
 });
-
-createUsers().then(() => {process.exit(0)}).catch(err => process.exit(1));

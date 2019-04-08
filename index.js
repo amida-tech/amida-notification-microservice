e6864662f8d093edd5b5d4f3b40165fa8d7f93e7
Grant Orndorff <grant@amida.com>
2019-04-08 17:06:36 -0400

    crash on fail to connect to mongo

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import 'babel-polyfill';
 import app from './config/express';
 import config from './config/config';
 import logger from './config/winston';
+import mongoConn from './config/mongo';
 
 // module.parent check is required to support mocha watch
 if (!module.parent) {
-    // listen on port config.port
-    app.listen(config.port, () => {
-        logger.info(`server started on port ${config.port} (${config.env})`, {
-            port: config.port,
-            node_env: config.env,
+    // establish connection to mongo
+    mongoConn.then(() => {
+        // listen on port config.port
+        app.listen(config.port, () => {
+            logger.info(`server started on port ${config.port} (${config.env})`, {
+                port: config.port,
+                node_env: config.env,
+            });
         });
+    }).catch((err) => {
+        logger.error(err);
+        process.exit(1);
     });
 }
 

1a1f2c6085363ea6f59960ce80de30eca422c19d
Grant Orndorff <grant@amida.com>
2019-04-08 16:29:17 -0400

    fixed error throwing to be better and use curly braces - will still have to change for verror

--- server/controllers/notifications.controller.js
+++ server/controllers/notifications.controller.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
+import httpStatus from 'http-status';
 import { User } from '../models/user.model';
 import { sendPushNotification } from '../helpers/pushNotificationHelper';
+import APIError from '../helpers/APIError';
 
 export async function sendPushNotifications(req, res) {
     const { pushData } = req.body;
@@ -12,8 +14,12 @@ export async function sendPushNotifications(req, res) {
             $in: usernames,
         },
     });
+
     // TODO how are we doing errors?
-    if (!users) throw new Error('USERS_NOT_FOUND');
+    if (!users) {
+        throw new APIError('USERS_NOT_FOUND', httpStatus.BAD_REQUEST, true);
+    }
+
     users.forEach((user) => {
         const userData = pushData.find(_userData => _userData.username === user.username);
         sendPushNotification(user, userData);

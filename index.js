d0c4a8e5a54b7366e2198db2b3fcfa27d85a6c49
Grant Orndorff <grant@amida.com>
2019-04-08 16:23:47 -0400

    removing bluebird

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import app from './config/express';
 import config from './config/config';
 import logger from './config/winston';
 
-// make bluebird default Promise
-// Promise = require('bluebird'); // eslint-disable-line no-global-assign
-
 // module.parent check is required to support mocha watch
 if (!module.parent) {
     // listen on port config.port
